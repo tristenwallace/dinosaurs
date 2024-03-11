80fa002b5b83f7d9e76142418932c77293fb7a93
tristenwallace <tri3sten@gmail.com>
2024-03-11 14:57:30 -0400

    feat: Create array of dino objects

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+// Dinos Constructor
+class Dino {
+  constructor({ species, weight, height, diet, where, when, fact }) {
+    this.species = species;
+    this.weight = weight;
+    this.height = height;
+    this.diet = diet;
+    this.where = where;
+    this.when = when;
+    this.fact = fact;
+  }
+}
+
 // Async load dinos data
 fetch('./dino.json')
-  .then(response => response.json())
-  .then(data => {
-    const Dinos = data.Dinos;
+  .then((response) => response.json())
+  .then((data) => {
+    const Dinos = data.Dinos.map((dinoData) => new Dino(dinoData));
     console.log(Dinos);
-    // You can now use your Dinos array as needed
   })
-  .catch(error => console.error('Error fetching JSON:', error));
+  .catch((error) => console.error('Error fetching JSON:', error));
